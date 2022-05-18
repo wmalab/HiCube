c1f4d72cffb7aa34147d92211b7ee1173009ebcc
Tiantian Ye <tye002@ucr.edu>
2022-05-18 08:01:43 -0700

    create zoom-in view and sync zoom-in views

--- src/App.js
+++ src/App.js
@@ -14,8 +14,13 @@ export default function App() {
     fromId: null,
   });
 
-  // const [selectMode, setSelectMode] = useState(false);
   const [mouseTool, setMouseTool] = useState("move");
+  const [rangeSelection, setRangeSelection] = useState({
+    type: null,
+    xDomain: null,
+    yDomain: null,
+    fromId: null,
+  });
 
   const locationChangeHandler = (location, id) => {
     console.log(id, "location change");
@@ -23,16 +28,24 @@ export default function App() {
     setMainLocation({ ...location, fromId: id });
   };
 
-  // const toggleSelectModeHandler = () => {
-  //   setSelectMode((prevSelectMode) => !prevSelectMode);
-  // };
-  const toggleMouseToolHandler = () => {
-    setMouseTool((prevMouseTool) => {
-      if (prevMouseTool === "move") {
-        return "select";
-      }
-      return "move";
-    });
+  const activateSelectHandler = () => {
+    setMouseTool("select");
+  };
+
+  const createZoomInHandler = () => {
+    setMouseTool("move_create");
+  };
+
+  const cancelSelectHandler = () => {
+    setMouseTool("move_cancel");
+  };
+
+  const clearSelectHandler = () => {
+    setMouseTool("move_clear");
+  };
+
+  const rangeSelectionChangeHandler = (type, location, id) => {
+    setRangeSelection({ ...location, fromId: id, type: type });
   };
 
   return (
@@ -45,18 +58,27 @@ export default function App() {
         {mainLocation.yDomain &&
           `yDomain: ${mainLocation.yDomain[0]}--${mainLocation.yDomain[1]}`}
       </p>
-      {/* <button onClick={toggleSelectModeHandler}>
-        {selectMode ? "Select" : "Move"}
-      </button> */}
-      <button onClick={toggleMouseToolHandler}>{mouseTool}</button>
+      <p>
+        {rangeSelection.xDomain &&
+          `select xDomain: ${rangeSelection.xDomain[0]}--${rangeSelection.xDomain[1]}`}
+      </p>
+      <p>
+        {rangeSelection.yDomain &&
+          `select yDomain: ${rangeSelection.yDomain[0]}--${rangeSelection.yDomain[1]}`}
+      </p>
+      <button onClick={activateSelectHandler}>Select</button>
+      <button onClick={createZoomInHandler}>Create Zoom-In View</button>
+      <button onClick={cancelSelectHandler}>Cancel</button>
+      <button onClick={clearSelectHandler}>Clear</button>
       <HiGlassCase
         id="hgc1"
         options={options}
         viewConfig={viewConfig}
         mainLocation={mainLocation}
         onMainLocationChange={locationChangeHandler}
-        // selectMode={selectMode}
         mouseTool={mouseTool}
+        rangeSelection={rangeSelection}
+        onRangeSelection={rangeSelectionChangeHandler}
       />
       <HiGlassCase
         id="hgc2"
@@ -64,8 +86,9 @@ export default function App() {
         viewConfig={viewConfig}
         mainLocation={mainLocation}
         onMainLocationChange={locationChangeHandler}
-        // selectMode={selectMode}
         mouseTool={mouseTool}
+        rangeSelection={rangeSelection}
+        onRangeSelection={rangeSelectionChangeHandler}
       />
     </div>
   );
--- src/components/HiGlass/HiGlassCase.js
+++ src/components/HiGlass/HiGlassCase.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useReducer } from "react";
 
 import HiGlassWrapper from "./HiGlassWrapper";
 
 // TODO: may need useCallback for handler function
 // DONE: handle mainLocation sync
-// TODO: handle switch mode between default and select
-// TODO: handle create zoom-in view from selected range
-// TODO: handle zoomLocation sync
+// DONE: handle switch mode between default and select
+// DONE: handle create zoom-in view from selected range
+// DONE: handle zoomLocation sync
 
 const DEBOUNCE = true;
 const DEBOUNCE_TIME = 100;
@@ -16,10 +16,63 @@ const HiGlassCase = (props) => {
   console.log("HiGlassCase render");
 
   const [mainLocation, setMainLocation] = useState();
+  const [zoomLocation, setZoomLocation] = useState();
   const hgcRef = useRef();
   const notify = useRef(true);
   const notifyTimer = useRef();
-  // const [viewConfig, setViewConfig] = useState(props.viewConfig);
+  const zoomLocationListener = useRef();
+
+  const viewConfigReducer = (state, action) => {
+    if (action.type === "CREATE_ZOOM_VIEW") {
+      let vcf = hgcRef.current.api.getViewConfig();
+      if (vcf.views.length === 2) {
+        vcf.views.pop();
+        vcf.views[0].tracks.center[0].contents.pop();
+        // FIXME create zoom view when exist zoom view not working
+        // possibly zoomLocation triggered so the new zoom view
+        // revert back to the old location
+      }
+      const tempView = JSON.parse(JSON.stringify(vcf.views[0]));
+      const newView = {
+        ...tempView,
+        uid: "bb",
+        initialXDomain: action.xDomain,
+        initialYDomain: action.yDomain,
+        layout: {
+          w: 12,
+          h: 12,
+          x: 0,
+          y: 0,
+          moved: false,
+          static: false,
+        },
+      };
+      vcf.views.push(newView);
+      vcf.views[0].tracks.center[0].contents.push({
+        type: "viewport-projection-center",
+        uid: "my-track-id",
+        fromViewUid: "bb",
+        options: {
+          projectionFillOpacity: 0.3,
+          projectionStrokeColor: "black",
+          projectionFillColor: "black",
+          projectionStrokeOpacity: 0.3,
+          strokeWidth: 1,
+        },
+      });
+      return vcf;
+    } else if (action.type === "CLEAR_ZOOM_VIEW") {
+      let vcf = hgcRef.current.api.getViewConfig();
+      vcf.views.pop();
+      vcf.views[0].tracks.center[0].contents.pop();
+      return vcf;
+    }
+  };
+
+  const [viewConfig, dispatchViewConfig] = useReducer(
+    viewConfigReducer,
+    props.viewConfig
+  );
 
   /* 
   listen to location changes for the main view
@@ -86,12 +139,101 @@ const HiGlassCase = (props) => {
     }, DISABLE_NOTIFY_TIME);
   }, [props.mainLocation]);
 
+  useEffect(() => {
+    if (props.mouseTool === "move_create") {
+      const { dataRange } = hgcRef.current.api.getRangeSelection();
+      if (!dataRange || dataRange.every((e) => e === null)) {
+        return;
+      }
+      if (dataRange.length === 2) {
+        props.onRangeSelection(
+          "CREATE",
+          {
+            xDomain: [...dataRange[0]],
+            yDomain: [...dataRange[1]],
+          },
+          props.id
+        );
+      }
+    } else if (props.mouseTool === "move_clear") {
+      props.onRangeSelection(
+        "CLEAR",
+        { xDomain: [null, null], yDomain: [null, null] },
+        props.id
+      );
+    }
+  }, [props.mouseTool]);
+
+  useEffect(() => {
+    console.log(props.rangeSelection);
+
+    if (!props.rangeSelection) {
+      return;
+    }
+
+    if (props.rangeSelection.type === "CREATE") {
+      dispatchViewConfig({
+        type: "CREATE_ZOOM_VIEW",
+        xDomain: props.rangeSelection.xDomain,
+        yDomain: props.rangeSelection.yDomain,
+      });
+    } else if (props.rangeSelection.type === "CLEAR") {
+      dispatchViewConfig({ type: "CLEAR_ZOOM_VIEW" });
+    } else if (props.rangeSelection.type === "UPDATE") {
+      const { xDomain, yDomain, fromId } = props.rangeSelection;
+      if (!fromId || fromId === props.id || !xDomain || !yDomain) {
+        return;
+      }
+      if (notifyTimer.current) {
+        clearTimeout(notifyTimer.current);
+      }
+      notify.current = false;
+      hgcRef.current.api.zoomTo("bb", ...xDomain, ...yDomain, 1);
+      notifyTimer.current = setTimeout(() => {
+        notify.current = true;
+        notifyTimer.current = null;
+      }, DISABLE_NOTIFY_TIME);
+    }
+  }, [props.rangeSelection]);
+
+  useEffect(() => {
+    if (viewConfig.views.length === 2 && !zoomLocationListener.current) {
+      zoomLocationListener.current = hgcRef.current.api.on(
+        "location",
+        (location) => {
+          setZoomLocation(location);
+        },
+        "bb"
+      );
+    } else if (viewConfig.views.length === 1 && zoomLocationListener.current) {
+      hgcRef.current.api.off("location", zoomLocationListener.current, "bb");
+      zoomLocationListener.current = null;
+    }
+  }, [viewConfig]);
+
+  useEffect(() => {
+    console.log(props.id, notify.current);
+
+    if (!notify.current) {
+      return;
+    }
+    if (!DEBOUNCE) {
+      props.onRangeSelection("UPDATE", zoomLocation, props.id);
+      return;
+    }
+    const timer = setTimeout(() => {
+      props.onRangeSelection("UPDATE", zoomLocation, props.id);
+    }, DEBOUNCE_TIME);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [zoomLocation]);
 
   return (
     <HiGlassWrapper
       onRef={hgcRef}
       options={props.options}
-      viewConfig={props.viewConfig}
+      viewConfig={viewConfig}
       mouseTool={props.mouseTool}
     />
   );
