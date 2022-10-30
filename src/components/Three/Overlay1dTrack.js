b4113dde3b00718bb25a5109abbd4238f5cc972f
Tiantian Ye <tye002@ucr.edu>
2022-10-29 23:48:01 -0700

    fix when 1d annotation len is 1
    when 1D annotation length is 1, anchor 2 not showing

--- src/components/Three/Overlay1dTrack.js
+++ src/components/Three/Overlay1dTrack.js
@@ -13,9 +13,9 @@ const Overlay1dTrack = (props) => {
           {options.anchor1Label && (
             <Html
               style={{
-                "font-size": options.anchor1LabelSize,
+                fontSize: options.anchor1LabelSize,
                 color: options.anchor1LabelColor,
-                "font-weight": options.anchor1LabelWeight,
+                fontWeight: options.anchor1LabelWeight,
               }}
             >
               <span>{options.anchor1Label}</span>
@@ -23,16 +23,16 @@ const Overlay1dTrack = (props) => {
           )}
         </mesh>
       )}
-      {options.drawAnchor2 && points.length > 1 && (
+      {options.drawAnchor2 && points.length > 0 && (
         <mesh position={points[points.length - 1]}>
           <sphereGeometry args={[+options.anchor2Radius, 16, 16]} />
           <meshBasicMaterial color={options.anchor2Color} />
           {options.anchor2Label && (
             <Html
               style={{
-                "font-size": options.anchor2LabelSize,
+                fontSize: options.anchor2LabelSize,
                 color: options.anchor2LabelColor,
-                "font-weight": options.anchor2LabelWeight,
+                fontWeight: options.anchor2LabelWeight,
               }}
             >
               <span>{options.anchor2Label}</span>
